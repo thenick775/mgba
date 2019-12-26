22350b21ff7aef7dfa985ebe88c64384005abf37
Vicki Pfau <vi@endrift.com>
2019-12-26 14:10:34 -0800

    WASM: Better modularization

---
+++ src/platform/wasm/pre.js
@@ -0,0 +1,16 @@
+Module.loadFile = (function() {
+	var loadGame = cwrap('loadGame', 'number', ['string']);
+	return function(name) {
+		if (loadGame(name)) {
+			var arr = name.split('.');
+			arr.pop();
+			Module.gameName = name;
+			Module.saveName = arr.join('.') + '.sav';
+			return true;
+		}
+		return false;
+	}
+})();
+Module.getSave = function() {
+	return FS.readFile('/data/saves/' + Module.saveName);
+}

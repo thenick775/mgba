ebb06411cb4b227031cab584298d2b3471b58c86
Vicki Pfau <vi@endrift.com>
2023-09-19 18:25:06 -0700

    WASM: Modernize syntax, add save/load state slots

--- src/platform/wasm/pre.js
+++ src/platform/wasm/pre.js
@@ -1,6 +1,12 @@
-Module.loadFile = (function() {
+/* Copyright (c) 2013-2023 Jeffrey Pfau
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+Module.loadFile = (name) => {
 	var loadGame = cwrap('loadGame', 'number', ['string']);
-	return function(name) {
+	Module.loadFile = (name) => {
 		if (loadGame(name)) {
 			var arr = name.split('.');
 			arr.pop();
@@ -10,7 +16,31 @@ Module.loadFile = (function() {
 		}
 		return false;
 	}
-})();
-Module.getSave = function() {
+	return Module.loadFile(name);
+};
+
+Module.getSave = () => {
 	return FS.readFile('/data/saves/' + Module.saveName);
 }
+
+Module.saveStateSlot = (slot, flags) => {
+	var saveStateSlot = cwrap('saveStateSlot', 'number', ['number', 'number']);
+	Module.saveStateSlot = (slot, flags) => {
+		if (flags === undefined) {
+			flags = 0b111111;
+		}
+		return saveStateSlot(slot, flags);
+	}
+	return Module.saveStateSlot(slot, flags);
+};
+
+Module.loadStateSlot = (slot, flags) => {
+	var loadStateSlot = cwrap('loadStateSlot', 'number', ['number', 'number']);
+	Module.loadStateSlot = (slot, flags) => {
+		if (flags === undefined) {
+			flags = 0b111101;
+		}
+		return loadStateSlot(slot, flags);
+	}
+	return Module.loadStateSlot(slot, flags);
+};
